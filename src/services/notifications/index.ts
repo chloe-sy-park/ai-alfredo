51e863bdfd11d5d95b8d0fcf16c916f5f9554b10
chloe-sy-park <74633862+chloe-sy-park@users.noreply.github.com>
2026-01-12 17:33:10 +0900

    feat: notifications index export

---
+++ src/services/notifications/index.ts
@@ -0,0 +1,3 @@
+export { notificationService, default } from './notificationService';
+export type { NotificationSettings, NotificationPermission } from './notificationService';
+export * from './notificationTemplates';
