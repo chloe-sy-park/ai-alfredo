a28d623ed92dbf312e889f551827350e60ab856c
chloe-sy-park <74633862+chloe-sy-park@users.noreply.github.com>
2026-01-12 19:49:21 +0900

    🔧 Fix: AlfredoAvatar export 추가 (빌드 에러 수정)

--- src/components/common/index.js
+++ src/components/common/index.js
@@ -2,3 +2,6 @@
 export { default as AlfredoNudge } from './AlfredoNudge';
 export { default as FailureCareSystem, DayEndModal } from './FailureCareSystem';
 export { default as EmptyState, TasksEmptyState, InboxEmptyState, EventsEmptyState, FocusEmptyState, SearchEmptyState } from './EmptyState';
+
+// Character 컴포넌트 re-export
+export { default as AlfredoAvatar } from '../character/AlfredoAvatar';
