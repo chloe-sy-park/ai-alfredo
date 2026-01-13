a92c879b91df6b90547ca0e08004da0011667338
chloe-sy-park <74633862+chloe-sy-park@users.noreply.github.com>
2026-01-13 19:49:08 +0900

    🔧 refactor: add settings index

---
+++ src/components/settings/index.js
@@ -0,0 +1,9 @@
+// Settings 컴포넌트 exports
+export { ToggleSwitch, SettingItem, SettingsCard, getThemeColors } from './settingsComponents';
+export { default as NotificationSection } from './NotificationSection';
+export { default as GoogleDriveSection } from './GoogleDriveSection';
+export { default as GmailSection } from './GmailSection';
+export { default as FocusModeSection } from './FocusModeSection';
+export { default as ConnectionSection, GoogleAccountConnection, ConnectionItem } from './ConnectionSection';
+export { AppInfoSection, AppInstallSection, DataManagementSection, AlfredoCard } from './AppInfoSection';
+export { default as SettingsPage } from './SettingsPage';
