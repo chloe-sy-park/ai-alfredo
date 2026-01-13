4db7057d1ede8e53f9858bef738e5d761b5bb64f
chloe-sy-park <74633862+chloe-sy-park@users.noreply.github.com>
2026-01-14 02:24:49 +0900

    Fix: main.tsx 심플화

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,10 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './styles/globals.css';
 
-// Zustand store 초기화
-import { useAuthStore } from './stores/authStore';
-
-// 앱 마운트 전 인증 초기화
-useAuthStore.getState().initialize();
-
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 );
