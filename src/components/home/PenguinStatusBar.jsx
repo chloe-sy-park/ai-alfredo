9e0c0129f568383a4d9b6f29701a6cf38331d8ee
chloe-sy-park <74633862+chloe-sy-park@users.noreply.github.com>
2026-01-13 22:35:59 +0900

    🐧 feat: add PenguinStatusBar as alias for AlfredoStatusBar

---
+++ src/components/home/PenguinStatusBar.jsx
@@ -0,0 +1,7 @@
+// PenguinStatusBar - AlfredoStatusBar의 alias
+// 펭귄 캐릭터 상태바 컴포넌트
+
+import AlfredoStatusBar from './AlfredoStatusBar';
+
+export var PenguinStatusBar = AlfredoStatusBar;
+export default AlfredoStatusBar;
