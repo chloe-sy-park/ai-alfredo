a8793252519f18d6f295d29b6bc403728bf5466a
chloe-sy-park <74633862+chloe-sy-park@users.noreply.github.com>
2026-01-14 22:21:21 +0900

    feat: life 컴포넌트 index에 새 컴포넌트들 추가

--- src/components/life/index.ts
+++ src/components/life/index.ts
@@ -2,3 +2,7 @@ export { default as WellbeingStatus } from './WellbeingStatus';
 export { default as StatusCards } from './StatusCards';
 export { default as GentleNudge } from './GentleNudge';
 export { default as LifeTrends } from './LifeTrends';
+export { HabitTracker } from './HabitTracker';
+export { WellnessCheckin } from './WellnessCheckin';
+export { HabitModal } from './HabitModal';
+export { MoodTrends } from './MoodTrends';
