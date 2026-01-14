91fd8689696d9b1c7a5c9dcae6600fc4bc7eba1c
chloe-sy-park <74633862+chloe-sy-park@users.noreply.github.com>
2026-01-14 20:25:08 +0900

    fix: 🐛 Fix export errors in component indices
    - Remove Toast default export, use * export
    - Remove FloatingBar export (file doesn't exist)

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -3,7 +3,7 @@ export { default as IntensityBadge } from './IntensityBadge';
 export { default as Button } from './Button';
 export { default as RingProgress } from './RingProgress';
 export { default as LoadingSpinner } from './LoadingSpinner';
-export { default as Toast } from './Toast';
 export { default as EventModal } from './EventModal';
 export * from './Skeleton';
 export * from './SuccessFeedback';
+export * from './Toast';
--- src/components/layout/index.ts
+++ src/components/layout/index.ts
@@ -1,4 +1,3 @@
 export { default as AppShell } from './AppShell';
-export { default as FloatingBar } from './FloatingBar';
 export { default as PageHeader } from './PageHeader';
 export { default as PageTransition } from './PageTransition';
