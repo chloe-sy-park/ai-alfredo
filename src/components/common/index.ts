102491adc38da3b6e04e90b6d856ee792243d488
chloe-sy-park <74633862+chloe-sy-park@users.noreply.github.com>
2026-01-14 20:23:19 +0900

    fix: 🐛 Remove missing exports from common/index.ts

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -1,6 +1,9 @@
 export { default as Card, AlfredoCard } from './Card';
 export { default as IntensityBadge } from './IntensityBadge';
-export { default as SourceBadge } from './SourceBadge';
-export { default as TextArea } from './TextArea';
+export { default as Button } from './Button';
+export { default as RingProgress } from './RingProgress';
+export { default as LoadingSpinner } from './LoadingSpinner';
+export { default as Toast } from './Toast';
+export { default as EventModal } from './EventModal';
 export * from './Skeleton';
 export * from './SuccessFeedback';
