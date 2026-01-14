5b7102572ed2a997c1af9a4f40f0bac4795a6cbd
chloe-sy-park <74633862+chloe-sy-park@users.noreply.github.com>
2026-01-14 21:55:47 +0900

    feat: Work 페이지 컴포넌트 index에 새로운 컴포넌트 추가
    - ProjectTaskGroup: 프로젝트별 태스크 그룹
    - FocusTimer: 집중 타이머 (포모도로)
    - TaskModal: 태스크 추가/편집 모달

--- src/components/work/index.ts
+++ src/components/work/index.ts
@@ -2,3 +2,6 @@ export { default as WorkBriefing } from './WorkBriefing';
 export { default as FocusTask } from './FocusTask';
 export { default as WorkTimeline } from './WorkTimeline';
 export { default as IncomingSignals } from './IncomingSignals';
+export { ProjectTaskGroup } from './ProjectTaskGroup';
+export { FocusTimer } from './FocusTimer';
+export { TaskModal } from './TaskModal';
